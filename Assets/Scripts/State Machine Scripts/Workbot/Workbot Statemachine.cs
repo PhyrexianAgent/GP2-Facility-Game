1c776f9376cdef22c95b9ec3a18b2635a105ab75
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-12-11 16:25:29 -0500

    Made Basic Workbot Prefab

--- Assets/Scripts/State Machine Scripts/Workbot/Workbot Statemachine.cs
+++ Assets/Scripts/State Machine Scripts/Workbot/Workbot Statemachine.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.AI;
 
 public class WorkbotStatemachine : StateMachine
 {
+    [SerializeField] private Transform testMovePoint;
     private SoundListener soundListener;
+    private NavMeshAgent navAgent;
     //[SerializeField] private UnityEvent<Sound> soundEvent;
     void Awake(){
         soundListener = GetComponent<SoundListener>();
+        navAgent = GetComponent<NavMeshAgent>();
+
     }
     void Start()
     {
         Working working = new Working(gameObject, soundListener.GetUnityEvent());
 
         AddNode(working, true);
+
+        navAgent.SetDestination(testMovePoint.position);
     }
 
     // Update is called once per frame
