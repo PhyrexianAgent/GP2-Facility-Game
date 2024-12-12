67b44a6c6353753d49d881a152a518508480c78c
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-12-12 16:32:54 -0500

    Added Editor Script for Camera State Machine
    Allows you to preview the camera looking at one of the points given in the point list. Note will not reset to original rotation (tried and failed to get working).

--- Assets/Scripts/State Machine Scripts/Turret Camera/Turret Camera.cs
+++ Assets/Scripts/State Machine Scripts/Turret Camera/Turret Camera.cs
@@ -8,6 +8,7 @@ public class TurretCamera : StateMachine
     [SerializeField] Transform[] lookPoints; // Camera will look between a bunch of points in this list (makes designing where camera looks easier)
     [SerializeField, Min(0)] float baseTurnrate, seenPlayerTurnRate, lookDuration;
     [SerializeField] private ConeDetector coneDetector;
+    [SerializeField] private LayerMask playerMask;
     void Start()
     {
         InitializeStates();
@@ -19,6 +20,15 @@ public class TurretCamera : StateMachine
         AddNode(looking, true);
         AddNode(seeingPlayer);
 
-        AddTransition(looking, seeingPlayer, new Predicate(() => coneDetector.PlayerInSpotlight(GameManager.GetPlayerTransform())));
+        AddTransition(looking, seeingPlayer, new Predicate(() => CanSeePlayer()));
     }
+    public Transform[] GetLookPoints() => lookPoints;
+    public void RotateToPoint(Vector3 point){
+        Vector3 diff = point - cameraHead.position;
+        //Vector3 newDirection = Vector3.RotateTowards(cameraHead.forward, diff, turnRate * Time.deltaTime, 0.0f);
+        cameraHead.rotation = Quaternion.LookRotation(diff.normalized);
+    }
+
+    private bool CanSeePlayer() => coneDetector.PlayerInSpotlight(GameManager.GetPlayerTransform()) && PlayerNotObstructed();
+    private bool PlayerNotObstructed() => !Physics.Linecast(cameraHead.position, GameManager.GetPlayerTransform().position, playerMask);
 }
---
+++ Assets/Scripts/TurretCameraEditor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(TurretCamera))]
+public class TurretCameraEditor : Editor
+{
+    private bool togglePointButtons;
+    private bool justToggled = false;
+    private Quaternion rotation;
+    public override void OnInspectorGUI(){
+        TurretCamera script = (TurretCamera)target;
+        Transform[] points = script.GetLookPoints();
+        DrawDefaultInspector();
+        togglePointButtons = GUILayout.Toggle(togglePointButtons, "Show Rotate To Point Buttons");
+        if (togglePointButtons){
+            if (!justToggled){
+                justToggled = true;
+                rotation = script.transform.rotation;
+            }
+            for (int i=0; i<points.Length; i++){
+                if (GUILayout.Button($"Look to Point {i+1}")) script.RotateToPoint(points[i].position);
+            }
+        }
+        else{
+            justToggled = false;
+            script.transform.rotation = rotation;
+        }
+    }
+}
