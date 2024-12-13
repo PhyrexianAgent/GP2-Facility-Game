06a947d001a98f1746a0e837955cd881119531c1
Boothaa <mbooth7@my.centennialcollege.ca>
2024-12-13 16:53:41 -0500

    SceneHandling
    Scene transition fades in and out

---
+++ Assets/Scripts/SceneHandling/SceneControl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    [SerializeField] private float fadeDuration;
+    private SceneFader sceneFade;
+
+
+    private void Awake()
+    {
+        sceneFade = GetComponentInChildren<SceneFader>();
+    }
+
+    public void loadScene(string sceneName)
+    {
+        StartCoroutine(sceneLoader(sceneName));
+    }
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        yield return sceneFade.fadeIn(fadeDuration);
+    }
+    private IEnumerator sceneLoader(string sceneName)
+    {
+        yield return sceneFade.fadeOut(fadeDuration);
+        yield return SceneManager.LoadSceneAsync(sceneName);
+    }
+    //purely for test purposes
+    private void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            loadScene("Level 1");
+        }
+        else if (Input.GetKey("2"))
+        {
+            loadScene("Level 2");
+        }
+        else if (Input.GetKey("3"))
+        {
+            loadScene("Level 3");
+        }
+        else if (Input.GetKey("m"))
+        {
+            loadScene("MainMenu");
+        }
+    }
+
+
+}
