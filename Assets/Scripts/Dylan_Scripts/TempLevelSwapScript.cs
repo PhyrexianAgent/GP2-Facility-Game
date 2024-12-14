76c909f57d47ff8a3d2b6507b98373b0ec3d3527
MrBobLoblaw <divern@outlook.com>
2024-12-14 11:36:39 -0500

    Level Swapping to Debug for now

---
+++ Assets/Scripts/Dylan_Scripts/TempLevelSwapScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempLevelSwapScript : MonoBehaviour
+{
+    [Tooltip("Pick the build index of the level you wish to load")]
+    [SerializeField] public int nextLevelIndex = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int sceneToLoad = nextLevelIndex;
+
+            if (nextLevelIndex == -1)
+            {
+                Debug.Log("Exiting the application.");
+                Application.Quit();
+
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #endif
+
+                return;
+            }
+
+            if (sceneToLoad < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.LogWarning("Specified scene index is out of range or no more scenes in the build settings to load.");
+            }
+        }
+    }
+}
