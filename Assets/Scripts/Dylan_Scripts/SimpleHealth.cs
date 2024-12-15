c2f09523f89ac816e5f05820f98364a1194b2fab
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-12-15 17:39:54 -0500

    Health Regeneration Works Properly
    Note I made regen rate multiplied by delta time becuase if I don't, frame rate can dictate amount of health regenerated.

--- Assets/Scripts/Dylan_Scripts/SimpleHealth.cs
+++ Assets/Scripts/Dylan_Scripts/SimpleHealth.cs
@@ -6,24 +6,22 @@ using UnityEngine.UI;
 
 public class SimpleHealth : MonoBehaviour
 {
-
-    public float health = 100f;
-    private float maxHealth;
-    public float visualHealth = 60f;
+    [SerializeField] private float maxHealth = 100;
+    //public float visualHealth = 60f;
     public float regenRate = 0.1f;
     [SerializeField, Min(0)] private float regenDelay = 0.3f;
-    private float regenTimer = 0f;
     private bool died = false;
     private Coroutine regenWait;
-
+    private float health;
+    private bool canRegenHealth = true;
     private CanvasGroup healthCanvasGroup;
 
     private void Start()
     {
+        health = maxHealth;
         healthCanvasGroup = GetComponent<CanvasGroup>();
         GameManager.SetPlayerHealth(this);
         maxHealth = health;
-        visualHealth = (maxHealth / 100) * visualHealth;
     }
 
     private void Update()
@@ -31,12 +29,17 @@ public class SimpleHealth : MonoBehaviour
         if (died) 
             healthCanvasGroup.alpha = 1;
         else{
-            if (health < maxHealth && regenWait == null) health += regenRate;
-            healthCanvasGroup.alpha = 1 - (health / visualHealth);
+            if (health < maxHealth && canRegenHealth){
+                health += regenRate * Time.deltaTime;
+            }
+            if (health > maxHealth) health = maxHealth;
+            healthCanvasGroup.alpha = 1 - (health / maxHealth);
         }
     }
     private IEnumerator DelayRegen(){
+        canRegenHealth = false;
         yield return new WaitForSeconds(regenDelay);
+        canRegenHealth = true;
     }
     private void RefreshRegenDelay(){
         if (regenWait != null) StopCoroutine(regenWait);
