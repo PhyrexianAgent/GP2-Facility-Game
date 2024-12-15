e96f21d0e54478f7eae80c2218b912e340f79250
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-12-15 02:31:28 -0500

    Made Health Code Better, Not Immediatly Restart Level and Properly Sized
    You can use the canvas group component to handle the alpha value of a ui element and its children, and even better that it is between 0 and 1 simplifying the math.

--- Assets/Scripts/Dylan_Scripts/SimpleHealth.cs
+++ Assets/Scripts/Dylan_Scripts/SimpleHealth.cs
@@ -11,9 +11,12 @@ public class SimpleHealth : MonoBehaviour
     private float maxHealth;
     public float visualHealth = 60f; // 0 - 100
     public float regenRate = 0.1f;
+    [SerializeField, Min(0)] private float regenDelay = 0.3f;
     private float regenTimer = 0f;
+    private bool died = false;
+    private Coroutine regenWait;
 
-    private Image healthVisual;
+    private CanvasGroup healthCanvasGroup;
 
     private void Start()
     {
@@ -21,40 +24,31 @@ public class SimpleHealth : MonoBehaviour
         GameManager.SetPlayerHealth(this);
         maxHealth = health;
         visualHealth = (maxHealth / 100) * visualHealth;
-        //health = 1;
     }
 
     private void Update()
     {
-        if (regenTimer < 0.3f)
-        {
-            regenTimer += Time.deltaTime;
+        if (died) 
+            healthCanvasGroup.alpha = 1;
+        else{
+            if (health < maxHealth && regenWait == null) health += regenRate;
+            healthCanvasGroup.alpha = 1 - (health / visualHealth);
         }
-        else
-        {
-            float alpha = ((150 / 100) * (1 - (health / visualHealth)));
-            Debug.Log(alpha);
-            healthVisual.color = new Color(healthVisual.color.r, healthVisual.color.g, healthVisual.color.b, alpha);
-            if (health < maxHealth)
-            {
-                health += regenRate;
-            }
-            regenTimer = 0f;
-        }
+    }
+    private IEnumerator DelayRegen(){
+        yield return new WaitForSeconds(regenDelay);
+    }
+    private void RefreshRegenDelay(){
+        if (regenWait != null) StopCoroutine(regenWait);
+        regenWait = StartCoroutine(DelayRegen());
     }
 
     public void DealDamage(float damage)
     {
-        if (health > 0)
-        {
+        if (!died){
             health -= damage;
-            if (health < visualHealth)
-            {
-                if (health < 0)
-                {
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-                }
-            }
+            RefreshRegenDelay();
+            died = health <= 0;
         }
     }
 
